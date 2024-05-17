6bc6959ada2ce4736ff8618bbeb4db6f538ab167
raulianee <raulianeestefani@gmail.com>
2024-05-17 14:21:37 -0300

    Update SweetAlert.js
    arrumando do erro do sweetAlert

--- src/components/SweetAlert.js
+++ src/components/SweetAlert.js
@@ -8,37 +8,37 @@ import {
 } from 'reactstrap';
 import './SweetAlert.components.css'
 import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+// import withReactContent from 'sweetalert2-react-content'
 
-const Swal = withReactContent(Swal)
 
 
 function Sweet() {
-
+ 
+  function alert(){
+    Swal.fire({
+      title: "Já está indo embora?",
+      text: "Agende sua consulta",
+      icon: "question",
+      showCancelButton: true
+    }
+    ).then((result) => {
+      if (result.iDismissed) {
+        alert('cancelou');
+      }
+    }
+    )
+  }
 
   return (
     <div className=''>
 
-      <button onclick="alert()" >Agende sua consulta</button>
+      <button onClick={alert} >Agende sua consulta</button>
 
-      {/* <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.11.0/dist/sweetalert2.all.min.js"></script> */}
+     ,
 
-      <script>
-        function alert(){
-          Swal.fire({
-            title: "Já está indo embora?",
-            text: "Agende sua consulta",
-            icon: "question",
-            showCancelButton: true
-          }
-          ).then((result) => {
-            if (result.iDismissed) {
-              alert('cancelou');
-            }
-          }
-          )
-        }
-      </script>
+     
+        
+    
     </div>
   );
 }
