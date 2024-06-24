111259e5289a2fa6afdf3d23fb92d60635449cc5
Gustavo Pereira <gustavo767psal@gmail.com>
2024-06-24 16:55:35 -0300

    mudança na agenda

--- src/components/componentsADM/Calendario.js
+++ src/components/componentsADM/Calendario.js
@@ -2,39 +2,10 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
 import './Adm.components.css'
-
-
-
-
-// function Calendario() {
-//   function Alerta() {
-//     alert("Olá mundo")
-//   }
-
-//   let dateClick = new Calendario( {
-//     plugins: [ interactionPlugin ],
-//     dateClick: function(info) {
-//       alert('Clicked on: ' + info.dateStr);
-//       alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-//       alert('Current view: ' + info.view.type);
-//       // change the day's background color just for fun
-//       info.dayEl.style.backgroundColor = 'red';
-//     }
-//   });
-//   return (
-//     <FullCalendar
-//       plugins={[ dayGridPlugin ]}
-//       initialView="dayGridMonth"
-//       dateClick={Alerta}
-      
-      
-//     />
-//   )
-// }
-
-// export default Calendario
-
-
+import { Calendar } from '@fullcalendar/core'
+import timeGridPlugin from '@fullcalendar/timegrid'
+ 
+ 
 function Calendario() {
   // Define the `handleDateClick` function for clarity and reusability
   const handleDateClick = (info) => {
@@ -43,14 +14,14 @@ function Calendario() {
     alert('Current view: ' + info.view.type);
     info.dayEl.style.backgroundColor = 'red';
   };
-
+ 
   return (
     <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]} // Ensure both plugins are included
-      initialView="dayGridMonth"
-      dateClick={handleDateClick} // Pass the `handleDateClick` function
+      plugins={[dayGridPlugin, timeGridPlugin]} // Ensure both plugins are included
+      initialView="dayGridWeek"
+      //dateClick={handleDateClick} // Pass the `handleDateClick` function
     />
   );
 }
-
+ 
 export default Calendario; // Export the component for use
