1602ae9c09325431c778e660912e72ad08946cde
Inaldo Henrique <hinaldo264@gmail.com>
2024-06-30 17:44:50 -0300

    Ajustes na responsividade do MENU
    Ajuste na logo, menu em geral :D

--- src/components/NavA.js
+++ src/components/NavA.js
@@ -1,51 +1,54 @@
-import { Link } from 'react-router-dom'
-import React from 'react';
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import {
   Navbar,
   Nav,
   NavItem,
   Col,
+  NavbarToggler,
+  Collapse,
 } from 'reactstrap';
 import { FaRegUser } from "react-icons/fa";
-import './NavA.components.css'
+import './NavA.components.css';
 import ModalLogin from './ModalLogin';
 import { MdHealthAndSafety } from "react-icons/md";
-import Logo from './img/Logo.png'
+import Logo from './img/Logo.png';
 
 function NavA() {
-
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div className=''>
-      <Navbar id='menu' expand="md bg-light" className='p-5'>
-        <Col className='ms-5' >
-            <Link className='navbar-brand cor-texto' to="/"><img id='logo' src={Logo} ></img></Link>
+      <Navbar id='menu' expand="md" className='p-5 bg-light'>
+        <Col className='ms-md-5'>
+          <Link className='navbar-brand cor-texto' to="/"><img className='img-fluid' id='logo' src={Logo} alt='Logo' /></Link>
         </Col>
-
-        <Nav id='barra' className="me-auto d-md-row justify-content-around'" navbar>
-          <NavItem>
-            <Link id='roter' className='nav-link cor-texto' to="/">Início</Link>
-          </NavItem>
-          <NavItem>
-            <Link id='roter' className='nav-link cor-texto' to="/Plano">Plano</Link>
-          </NavItem>
-          <NavItem>
-            <Link id='roter' className='nav-link cor-texto' to="/Sobre">Sobre</Link>
-          </NavItem>
-          <NavItem>
-            <Link id='roter' className='nav-link cor-texto' to="/Contato">Contato</Link>
-          </NavItem>
-          <NavItem>
-            <Link id='roter' className='nav-link cor-texto' to="/Agende">Agende</Link>
-          </NavItem>
-          <NavItem>
-            <ModalLogin />
-          </NavItem>
-        </Nav>     
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar className="text-center mt-md-0 mt-5">
+          <Nav id='barra' className=" d-flex ms-auto justify-content-md-end justify-content-center align-items-center" navbar>
+            <NavItem>
+              <Link id='roter' className='nav-link cor-texto' to="/">Início</Link>
+            </NavItem>
+            <NavItem>
+              <Link id='roter' className='nav-link cor-texto' to="/Plano">Plano</Link>
+            </NavItem>
+            <NavItem>
+              <Link id='roter' className='nav-link cor-texto' to="/Sobre">Sobre</Link>
+            </NavItem>
+            <NavItem>
+              <Link id='roter' className='nav-link cor-texto' to="/Contato">Contato</Link>
+            </NavItem>
+            <NavItem>
+              <Link id='roter' className='nav-link cor-texto' to="/Agende">Agende</Link>
+            </NavItem>
+            <NavItem className='d-flex justify-content-center'>
+              <ModalLogin />
+            </NavItem>
+          </Nav>
+        </Collapse>
       </Navbar>
-
     </div>
-
   );
 }
 
