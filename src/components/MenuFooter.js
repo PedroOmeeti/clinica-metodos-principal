500bc490185833f229b251486497f7b0fdefc6cb
Inaldo Henrique <hinaldo264@gmail.com>
2024-07-01 19:08:31 -0300

    Responsividade da logo no footer

--- src/components/MenuFooter.js
+++ src/components/MenuFooter.js
@@ -4,22 +4,22 @@ import "./MenuFooter.components.css"
 import { FaWhatsapp, FaEnvelope, FaInstagram, FaFacebookSquare } from "react-icons/fa";
 import Logo from './img/logo branca.png'
 
-function MenuFooter(){
-    return(
+function MenuFooter() {
+    return (
         <div className='text-center text-white p-4 fundo'>
             <Container>
                 <Row>
                     <Row className='logosize mb-2'>
-                        <Col className='align-items-center d-flex justify-content-center'><img id='logo' src={Logo}  ></img></Col>
+                        <Col className='align-items-center d-flex justify-content-center'><img className='img-fluid' id='logofooter' src={Logo}  ></img></Col>
                     </Row>
                     <Row>
-                            <div className='mt-3'>
-                                <a href='mailto:metodosclinicarecepcao@outlook.com?subject=Fale conosco' target="_blank" className='me-3 p-1 fs-3'><FaEnvelope/></a>
-                                <a href='https://www.facebook.com/metodosclinica/' target="_blank" className='me-3 fs-3'><FaFacebookSquare/></a>
-                                <a href='https://www.instagram.com/metodosclinica/' target="_blank" className='me-3 fs-3'><FaInstagram/></a>
-                                <a href='https://api.whatsapp.com/send?phone=5512996448045&text=Ol%C3%A1!%20Gostaria%20de%20saber%20mais%20sobre%20os%20servi%C3%A7os%20da%20cl%C3%ADnica.
-' target="_blank" className='fs-3'><FaWhatsapp/></a>
-                            </div>
+                        <div className='mt-3'>
+                            <a href='mailto:metodosclinicarecepcao@outlook.com?subject=Fale conosco' target="_blank" className='me-3 p-1 fs-3'><FaEnvelope /></a>
+                            <a href='https://www.facebook.com/metodosclinica/' target="_blank" className='me-3 fs-3'><FaFacebookSquare /></a>
+                            <a href='https://www.instagram.com/metodosclinica/' target="_blank" className='me-3 fs-3'><FaInstagram /></a>
+                            <a href='https://api.whatsapp.com/send?phone=5512996448045&text=Ol%C3%A1!%20Gostaria%20de%20saber%20mais%20sobre%20os%20servi%C3%A7os%20da%20cl%C3%ADnica.
+' target="_blank" className='fs-3'><FaWhatsapp /></a>
+                        </div>
                     </Row>
                     <Row className='mt-2'>
                         <Col>
@@ -37,9 +37,9 @@ function MenuFooter(){
                             <a href='#' className='text-white'> Politica de Privacidade</a>
                         </Col>
                     </Row>
-                </Row>    
-            </Container>    
-        </div> 
+                </Row>
+            </Container>
+        </div>
     )
 }
 
--- src/components/NavA.js
+++ src/components/NavA.js
@@ -49,7 +49,6 @@ function NavA() {
               )}
               <ModalLogin />
             </NavItem>
-
           </Nav>
         </Collapse>
 
